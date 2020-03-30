25f6663a0e712f5d5a0a927c50310112d08c94ff
Cuboideb <cuboideb@gmail.com>
2020-03-30 17:17:30 -0300

    Moved alter command

--- app/src/main/java/org/rephial/xyangband/ButtonRibbon.java
+++ app/src/main/java/org/rephial/xyangband/ButtonRibbon.java
@@ -328,7 +328,7 @@ public class ButtonRibbon implements OnClickListener,
             atLeft.getChildAt(1).setVisibility(View.GONE);
             atLeft.getChildAt(2).setVisibility(View.GONE);
 
-            String txt = ".iUmhfvngdRb,l[]C+~LM=?";
+            String txt = ".iUmhfvngdR+b,l[]C~LM=?";
             for (char c: txt.toCharArray()) {
                 String label = Character.toString(c);
                 String action = "CMD_" + c;
