b3e6c1720ea3b548f36a811fa1e36024ab83eb6e
Cuboideb <cuboideb@gmail.com>
2021-08-21 08:55:36 -0300

    FA tiles

--- app/src/main/java/org/rephial/xyangband/Plugins.java
+++ app/src/main/java/org/rephial/xyangband/Plugins.java
@@ -35,11 +35,11 @@ final public class Plugins {
 		}
 		public boolean onlyText()
 		{
-			return this != angband; //&& this != faangband;
+			return this != angband && this != faangband;
 		}
 		public boolean only1x1()
 		{
-			return this != angband; //&& this != faangband;
+			return this != angband && this != faangband;
 		}
 		public boolean noMouse()
 		{
