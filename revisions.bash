4ee387bd498eaa22514f925a9bd8fc0608f6bca8
Cuboideb <cuboideb@gmail.com>
2022-01-03 19:20:07 -0300

    Helper script

---
+++ revisions.bash
@@ -0,0 +1,9 @@
+echo Sumary of revisions
+cd angband-github
+echo Vanilla: https://github.com/angband/angband/commit/`git rev-parse HEAD`
+cd ../faangband-github
+echo FAangband: https://github.com/NickMcConnell/FAangband/`git rev-parse HEAD`
+cd ../frog-github
+echo FrogComposband: https://github.com/sulkasormi/frogcomposband/commit/`git rev-parse HEAD`
+cd ../silq-github
+echo Sil-Q: https://github.com/sil-quirk/sil-q/commit/`git rev-parse HEAD`
