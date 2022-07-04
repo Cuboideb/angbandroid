5e568f3c520b71052ffa79686038d0abfacac770
Cuboideb <cuboideb@gmail.com>
2022-07-04 09:44:52 -0300

    V updates

--- app/src/main/cpp/angband/effect-handler-attack.c
+++ app/src/main/cpp/angband/effect-handler-attack.c
@@ -754,12 +754,6 @@ bool effect_handler_BREATH(effect_handler_context_t *context)
 			diameter_of_source = 25;
 	}
 
-	/* For the ANDROID port*/
-	if (z_info->max_range < 20 && diameter_of_source > 0) {
-		diameter_of_source = (z_info->max_range * diameter_of_source) / 20;
-		diameter_of_source = MAX(diameter_of_source, 1);
-	}
-
 	/* Breathe at the target */
 	if (project(context->origin, rad, target, dam, type, flg, degrees_of_arc,
 				diameter_of_source, context->obj))
@@ -826,12 +820,6 @@ bool effect_handler_ARC(effect_handler_context_t *context)
 	/* Max */
 	if (diameter_of_source > 25) {
 		diameter_of_source = 25;
-	}
-
-	/* For the ANDROID port*/
-	if (z_info->max_range < 20 && diameter_of_source > 0) {
-		diameter_of_source = (z_info->max_range * diameter_of_source) / 20;
-		diameter_of_source = MAX(diameter_of_source, 1);
 	}
 
 	/* Aim at the target */
@@ -1441,7 +1429,7 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 
 			/* Move player */
 			monster_swap(pgrid, safe_grid);
-			player_handle_post_move(player, true);
+			player_handle_post_move(player, true, true);
 		}
 
 		/* Take some damage */
@@ -1695,7 +1683,7 @@ bool effect_handler_JUMP_AND_BITE(effect_handler_context_t *context)
 
 	/* Move player */
 	monster_swap(player->grid, grid);
-	player_handle_post_move(player, true);
+	player_handle_post_move(player, true, false);
 
 	/* Now bite it */
 	drain = MIN(mon->hp + 1, amount);
